e8c8777d7c77c03fea553d391f315166e0341a85
Shikhar Singh <Shikharsingh226@gmail.com>
2021-05-11 14:21:19 +0530

    Add files via upload

---
+++ SpringDataRest/src/main/java/com/shikhar/demo/AlienRepo.java
@@ -0,0 +1,12 @@
+package com.shikhar.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.shikhar.demo.model.Alien;
+
+@RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
+public interface AlienRepo extends JpaRepository<Alien,Integer>
+{
+
+}
---
+++ SpringDataRest/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Shikhar','Java');
+insert into alien values (102,'Ravi','ML');
+insert into alien values (103,'Rajul','Python');
+insert into alien values (104,'Raj','CPP');
+insert into alien values (105,'shivam','C');
